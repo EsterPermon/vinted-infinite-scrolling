0402b97cecf7545118d6ea2ab48aa40cc5180832
ester-monteiro <ester.monteiro@bewatec.com>
2022-11-02 23:22:22 +0100

    Fetching each image on it's own ImageFrame component

--- src/components/ImageFrame/index.tsx
+++ src/components/ImageFrame/index.tsx
@@ -1,50 +1,57 @@
-import { useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import { RenderedImage } from "../../types/Gallery";
-import {
-  ImageFrameStyledContainer,
-  ImageOverlayStyledContainer,
-} from "./styles";
+import { ImageFrameStyledContainer } from "./styles";
+import { API_KEY, GET_PHOTO_URL, IMAGE_BASE_URL } from "../../utils/constants";
+import { getImageInfo } from "../../api/images";
+import ImageOverlay from "../ImageOverlay";
+import { useMemo } from "react";
 
 type ImageFrameProps = {
-  image: RenderedImage;
+  imageId: string;
 };
 
 const ImageFrame = (props: ImageFrameProps) => {
-  const { image } = props;
-  const { title, src, owner } = image;
+  const { imageId } = props;
+  const emptyImage = useMemo(() => {
+    return {
+      id: "",
+      title: "",
+      owner: "",
+      src: "",
+    };
+  }, []);
+  const [image, setImage] = useState<RenderedImage>(emptyImage);
   const [showOverlay, setShowOverlay] = useState(false);
 
-  return (
+  useEffect(() => {
+    fetchImageInfo(imageId);
+  }, []);
+
+  const fetchImageInfo = useCallback(async (imageId: string) => {
+    const photoURL = `${GET_PHOTO_URL}&api_key=${API_KEY}&photo_id=${imageId}&format=json&nojsoncallback=1`;
+    try {
+      const imageInfo = await getImageInfo(photoURL);
+      const { id, owner, title, server, secret } = imageInfo;
+      const image: RenderedImage = {
+        id,
+        title: title._content,
+        owner: owner.username,
+        src: `${IMAGE_BASE_URL}/${server}/${id}_${secret}.jpg`,
+      };
+      setImage(image);
+    } catch (e) {}
+  }, []);
+
+  return image.id ? (
     <ImageFrameStyledContainer
       onMouseEnter={() => setShowOverlay(true)}
       onMouseLeave={() => setShowOverlay(false)}
     >
-      {showOverlay && <ImageOverlay title={title} owner={owner} />}
-      <img src={src} alt={title} />
+      {showOverlay && <ImageOverlay image={image} />}
+      <img src={image.src} alt={image.title} />
     </ImageFrameStyledContainer>
-  );
-};
-
-type ImageOverlayProps = {
-  title: string;
-  owner: string;
-};
-
-const ImageOverlay = (props: ImageOverlayProps) => {
-  const defaultTitle = "No Title";
-  const defaultOwner = "Anonymous";
-  const { title, owner } = props;
-  const buttonLabel = "Favourite";
-
-  return (
-    <ImageOverlayStyledContainer>
-      <span className="image-info">
-        <span className="title">{title || defaultTitle}</span>
-        <hr />
-        <span className="owner">{owner || defaultOwner}</span>
-      </span>
-      <button>{buttonLabel}</button>
-    </ImageOverlayStyledContainer>
+  ) : (
+    <Fragment />
   );
 };
 
