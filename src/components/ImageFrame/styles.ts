46c79eaa4965d3e276784a5991976a42d6574cb2
ester-monteiro <ester.monteiro@bewatec.com>
2022-11-03 14:38:31 +0100

    Adding breakpoints and fitting image to container

--- src/components/ImageFrame/styles.ts
+++ src/components/ImageFrame/styles.ts
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 export const ImageFrameStyledContainer = styled.div`
   position: relative;
+  display: flex;
+  justify-content: center;
+  border: 1px solid gray;
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.1);
+
   img {
     border-radius: 5px;
     width: 100%;
     height: 100%;
+    object-fit: contain;
   }
 `;
--- src/containers/Gallery/styles.ts
+++ src/containers/Gallery/styles.ts
@@ -1,9 +1,47 @@
 import styled from "styled-components";
 
 export const GalleyStyledContainer = styled.div`
-  padding: 25px 30px;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-auto-rows: 250px;
-  grid-gap: 30px;
+  .grid {
+    min-height: 100vw;
+    padding: 25px 30px;
+    display: grid;
+    grid-auto-rows: 250px;
+    grid-gap: 30px;
+  }
+  .sentinel {
+    height: 20px;
+    font-size: 18px;
+    font-weight: bolder;
+    text-align: center;
+  }
+  @media (max-width: 425px) {
+    .grid {
+      grid-template-columns: 1fr;
+      grid-gap: 30px;
+    }
+  }
+
+  @media (min-width: 426px) and (max-width: 768px) {
+    .grid {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    .grid {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+
+  @media (min-width: 1025px) and (max-width: 1920px) {
+    .grid {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+  }
+
+  @media (min-width: 1921px) {
+    .grid {
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    }
+  }
 `;
